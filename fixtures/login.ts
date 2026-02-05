8c129b2a35005b850315e5f7c929a169a68c169b
judethecreator1123 <judethecreator@gmail.com>
2026-02-05 21:09:40 +0800

    Refactor login function to save auth.json path dynamically and improve error handling

--- fixtures/login.ts
+++ fixtures/login.ts
@@ -9,18 +9,20 @@ async function loginViaUi() {
     });
 
     const page = await browser.newPage();
-    
+
     // Navigate to project url
     await page.goto(process.env.BASEURL!);
 
     // Login
     try {
-        const loginPage = new LoginPage(page);
-        await loginPage.login(process.env.USERID!,process.env.PASSWORD! );
-        await page.context().storageState({ path: 'auth.json' });
-        console.log(`Login Successful! Session is saved`);
+        const fs = require('fs');
+        const authPath = path.resolve(__dirname, '../auth.json');
+        console.log('Saving auth.json to:', authPath);
+
+        await page.context().storageState({ path: authPath });
+        console.log('File exists?', fs.existsSync(authPath));
     } catch (error) {
-        console.log('Unable to login',error)
+        console.log('Unable to login', error)
     }
 
     await browser.close();
